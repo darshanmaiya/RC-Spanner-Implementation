07794a324760a2d15367acc0fa1a529bead55f77
suraj-rajesh <surajx.rajesh@gmail.com>
2016-03-02 22:38:15 -0800

    A basic implementation of Paxos Prepare stage

--- Paxos/src/main/java/PaxosServer.java
+++ Paxos/src/main/java/PaxosServer.java
@@ -13,8 +13,14 @@ public class PaxosServer {
 	private String 						ipAddress;
 	private int							port;
 	private ServerSocket				serverSocket;
-//	private int 						numPaxosServers;
-//	private HashMap<Integer, Socket>	clientSockets;
+	private Socket 						redisYcsbSocket;
+	private Server 						redisYcsbServer;
+	private int 						minProposal;
+	//	private List<Socket>				acceptorSocket;
+	//	private List<PrintWriter>			acceptorWriter;
+	//	private List<BufferedReader>		acceptorReader;
+	//	private int 						numPaxosServers;
+	//	private HashMap<Integer, Socket>	clientSockets;
 
 	// Getters and Setters
 	public String getState(){ return this.state; }
@@ -22,6 +28,9 @@ public class PaxosServer {
 	public int getId(){ return this.id; }
 	public void setLeader(Server leader) {this.leader = leader;}
 	public List<Server> getParticipants(){ return this.participants; }
+	public int getPort() { return this.port; };
+	public Socket getRedisYcsbSocket() {return this.redisYcsbSocket; }
+	public int getMinProposal() { return this.minProposal; }
 
 	// Constructors
 	public PaxosServer(){}
@@ -35,6 +44,7 @@ public class PaxosServer {
 		}else { this.state = "ACCEPTOR";}
 
 		this.maxRound = 0;
+		this.minProposal = 0;
 		this.participants = new ArrayList<Server>();
 	}
 
@@ -54,11 +64,17 @@ public class PaxosServer {
 				// Not this server
 				if (Integer.parseInt(serverInfo[0]) != this.id){			
 					Server newServer = new Server(Integer.parseInt(serverInfo[0]), serverInfo[1], Integer.parseInt(serverInfo[2]));
-					this.participants.add(newServer);
 
 					// If parsed Server is a LEADER
 					if (Integer.parseInt(serverInfo[0]) == 1){
 						this.leader = newServer;
+					}
+					// If parsed Server is a Redis YCSB server
+					else if (Integer.parseInt(serverInfo[0]) == 0){
+						this.redisYcsbServer = newServer;
+					}
+					else{
+						this.participants.add(newServer);
 					}
 				}
 				else{
@@ -78,38 +94,153 @@ public class PaxosServer {
 		// If: This Server itself is the LEADER
 		if (this.state == "LEADER"){
 			try {
-				this.serverSocket = new ServerSocket(this.port);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			// Wait for connections from ACCEPTOR PaxosServers
-			while(true){
-				try {
-					Socket clientSocket = null;
-					clientSocket = serverSocket.accept();
-					new Thread(new PaxosAcceptorHandler(clientSocket, this)).start();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		// Else: Server is an ACCEPTOR, connect to LEADER
-		else{
-			try{
-				Socket clientConnection = new Socket(leader.getIpAddress(), leader.getPort());
-				// Read Server
-				BufferedReader input = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
+				// Connect to redis YCSB server
+				this.redisYcsbSocket = new Socket(redisYcsbServer.getIpAddress(), redisYcsbServer.getPort());
 
-				String responseLine;
+				// Connect to all participants
+				for (Server acceptor : this.participants){
+
+					Socket newSocket = new Socket(acceptor.getIpAddress(), acceptor.getPort());
+					PrintWriter acceptorWrite = new PrintWriter(new OutputStreamWriter(newSocket.getOutputStream()));
+					BufferedReader acceptorRead = new BufferedReader(new InputStreamReader(newSocket.getInputStream()));
+					acceptor.setAcceptorSocket(newSocket);
+					acceptor.setAcceptorWriter(acceptorWrite);
+					acceptor.setAcceptorReader(acceptorRead);
+				}
+
+				// Wait for message from Redis YCSB server, send it to Paxos Acceptors 
+				String ycsbMessage;
+				PrintWriter ycsbWriter = new PrintWriter(new OutputStreamWriter(redisYcsbSocket.getOutputStream()));
+				BufferedReader ycsbReader = new BufferedReader(new InputStreamReader(redisYcsbSocket.getInputStream()));
+				String acceptorMessage;
+				int numPromises = 0;
 				
-				while ((responseLine = input.readLine()) != null)
-				System.out.println(String.valueOf(this.id) + " : Server : " + responseLine);
+				while (true){
+					
+					// When there is a message from YCSB server, initiate Paxos.
+					while ((ycsbMessage = ycsbReader.readLine()) != null){
+						
+						numPromises = 0;
+						int majority = (this.getParticipants().size() + 1)/2 + 1;
+
+						System.out.println("Read from YcSB : " + ycsbMessage);
+
+						// Broadcast PrepareRPC to all Acceptors
+						String prepareRPC = PaxosPrepareRPC();
+
+						// Phase 1
+						for (Server acceptor : this.participants){
+
+							// Check if Acceptor socket is still open
+							if (!acceptor.getAcceptorSocket().isClosed()){
+
+								// Send Paxos prepare message to Acceptors
+								acceptor.getAcceptorWriter().println(prepareRPC);
+								acceptor.getAcceptorWriter().flush();
+
+								// Read reply message from Acceptor
+								acceptorMessage = acceptor.getAcceptorReader().readLine();
+
+								// Check if PROMISE or NACK
+								if (acceptorMessage.equals("PROMISE"))
+								{
+									numPromises++;
+								}
+							}
+						}
+
+						// Testing
+						System.out.println("Number of Promises: " + numPromises);
+						
+						// Check if Commit quorum achieved
+						if (numPromises >= majority){
+							// Phase 2	
+						}
+						else
+						{
+							ycsbWriter.println("ABORT");
+							ycsbWriter.flush();
+						}
+					}
+				}
 			}catch (Exception e){
 				e.printStackTrace();
 			}
 		}
+		// Else: Server is an ACCEPTOR, open port for LEADER to connect
+		else{
+			try{
+				String leaderMessage;
+				this.serverSocket = new ServerSocket(this.port);
+				Socket leaderSocket = this.serverSocket.accept();
+
+				// Once connected to LEADER, listen for Paxos message from the LEADER
+				PrintWriter leaderWriter = new PrintWriter(new OutputStreamWriter(leaderSocket.getOutputStream()));
+				BufferedReader leaderReader = new BufferedReader(new InputStreamReader(leaderSocket.getInputStream()));
+
+				while (true)
+				{
+					leaderMessage = leaderReader.readLine();
+					System.out.println("Read from LEADER : " + leaderMessage);
+
+					//					if (leaderMessage.equals("HELLO")){
+					//						System.out.println("Sending message to leader...");
+					//						leaderWriter.println("Reply from " + "Server " + this.getId());
+					//						leaderWriter.flush();
+					//					}
+
+					// If PaxosPrepareRPC
+					if (leaderMessage.contains("PREPARE")){
+						String[] extract = leaderMessage.split("\\s+");
+
+						// Get maxRound
+						String maxRoundString = extract[0].substring(extract[0].indexOf("=")+1);
+						int receivedMaxRound = Integer.parseInt(maxRoundString);
+
+						// Get Server ID
+						String serverIdString = extract[1].substring(extract[1].indexOf("=")+1);
+						int serverID = Integer.parseInt(serverIdString);
+
+						// Check with LatestAcceptedProposal if received proposal is latest or not
+						// If received proposal is latest
+						if (receivedMaxRound > this.minProposal)
+						{	
+							this.minProposal = receivedMaxRound;
+							// Connect to 2PC module, check for its response, send PROMISE or NACK accordingly
+							leaderWriter.println("PROMISE");
+							leaderWriter.flush();
+							// Else send NACK
+						}
+						// Received proposal is not latest
+						else{
+							// Send NACK
+							leaderWriter.println("NACK");
+							leaderWriter.flush();
+						}
+					}
+
+					// If PaxosAcceptRPC
+					if (leaderMessage.contains("ACCEPT")){
+
+					}
+				}
+			}
+
+			catch (Exception e){
+				e.printStackTrace();
+			}
+		}
 	}
-	
+
+	// Generate Paxos PrepareRPC
+	private String PaxosPrepareRPC(){
+		// Prepare message will be of the form, "PREPARE=123 ID=1"
+		// Proposal no = "(maxRound + 1)" + "Server ID"
+		this.maxRound++;
+		String proposeMessage = "PREPARE=" + this.maxRound + " ID=" + this.id;
+		return proposeMessage;
+	}
+
 	public static void main(String args[]){
 		PaxosServer p1 = new PaxosServer(1);
 		p1.initialize();
--- Paxos/src/main/java/Server.java
+++ Paxos/src/main/java/Server.java
@@ -1,38 +1,35 @@
+import java.io.*;
+import java.net.Socket;
 
 public class Server {
 
 	private int id;
 	private String ipAddress;
 	private int port;
-	
-	public String getIpAddress() {
-		return ipAddress;
-	}
-	public void setIpAddress(String ipAddress) {
-		this.ipAddress = ipAddress;
-	}
-	public int getPort() {
-		return port;
-	}
-	public void setPort(int port) {
-		this.port = port;
-	}
-	
-	public int getId(){
-		return id;
-	}
-	
-	public void setId(int id){
-		this.id = id;
-	}
-	
+	private Socket acceptorSocket;
+	private PrintWriter acceptorWriter;
+	private BufferedReader acceptorReader;
+
 	// Server constructor
 	public Server(int id, String ipAddress, int port){
 		this.id = id;
 		this.ipAddress = ipAddress;
 		this.port = port;
 	}
-	
+
+	public void setIpAddress(String ipAddress) { this.ipAddress = ipAddress; }
+	public int getPort() { return port; }
+	public void setPort(int port) { this.port = port; }
+	public int getId(){ return id; }
+	public void setId(int id){ this.id = id; }
+	public String getIpAddress() { return ipAddress; }
+	public Socket getAcceptorSocket() { return acceptorSocket; }
+	public void setAcceptorSocket(Socket acceptorSocket) { this.acceptorSocket = acceptorSocket; }
+	public PrintWriter getAcceptorWriter() { return acceptorWriter; }
+	public void setAcceptorWriter(PrintWriter acceptorWriter) { this.acceptorWriter = acceptorWriter; }
+	public BufferedReader getAcceptorReader() { return acceptorReader; }
+	public void setAcceptorReader(BufferedReader acceptorReader) { this.acceptorReader = acceptorReader; }
+
 	public static void main(String args[]){
 		PaxosServer p2 = new PaxosServer(2);
 		p2.initialize();

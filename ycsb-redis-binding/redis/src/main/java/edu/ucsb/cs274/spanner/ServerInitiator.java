da7133bd00bd1dbfa1d27a9f76e75d9803d89f50
suraj-rajesh <surajx.rajesh@gmail.com>
2016-03-16 18:37:42 -0700

    Updated config.properties; Removed Server2Initiator and Server3Initiator

--- ycsb-redis-binding/redis/src/main/java/edu/ucsb/cs274/spanner/ServerInitiator.java
+++ ycsb-redis-binding/redis/src/main/java/edu/ucsb/cs274/spanner/ServerInitiator.java
@@ -3,21 +3,21 @@ package edu.ucsb.cs274.spanner;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server2Initiator {
-	
+public class ServerInitiator {
+
 	public static void main(String[] args){
 
 		try {
-			ServerSocket listener = new ServerSocket(5002);
+			ServerSocket listener = new ServerSocket(5001);
 
 			while(true) {
 				Socket request = listener.accept();
-				(new Thread(new Server(request, 2))).start();
+				(new Thread(new Server(request, 1))).start();
 			}
 		}
 		catch (Exception e){
 			e.printStackTrace();
 		}
 	}
-
 }
+
